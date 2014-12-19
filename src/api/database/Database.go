453f817458c8729e3ae57d03dc7653ecfdf9acbe
Ryan Armstrong <ryan@cavaliercoder.com>
2014-12-19 17:18:59 +0800

    Added documentation to database/database.go

--- src/api/database/Database.go
+++ src/api/database/Database.go
@@ -24,19 +24,25 @@ import (
 )
 
 type Driver interface {
-        Connect(*configuration.DatabaseConfig) error
-	Clone() (Driver, error)
-	Close() error
-	IsBootStrapped() (bool, error)
-	BootStrap(*configuration.Answers) error
-        GetAll(string, M, interface{}) error
-        GetOne(string, M, interface{}) error
-	GetOneById(string, interface{}, interface{}) error
-        Insert(string, interface{}) error
+        Connect(*configuration.DatabaseConfig) error		// Connect to the database
+	Clone() (Driver, error)					// Clone a database connection
+	Close() error						// Disconnect from the database
+	IsBootStrapped() (bool, error)				// Return true is datasbe schema is intialized
+	BootStrap(*configuration.Answers) error			// Initialize database schema
+        GetAll(string, M, interface{}) error			// Get multiple entities from the database
+        GetOne(string, M, interface{}) error			// Get a single entity from the database
+	GetOneById(string, interface{}, interface{}) error	// Get a single entity from the database by ID
+        Insert(string, interface{}) error			// Add an entity to the database
 }
 
+// M is a convenience shortcut for `map[string]interface{}`
 type M map[string]interface{}
 
+/*
+ * Connect loads application configuration to select a database driver, loads
+ * the driver and connects to the database using the specified connection
+ * configuration.
+ */
 func Connect() (Driver, error) {
 	var driver Driver
 	config, err := configuration.GetConfig()
